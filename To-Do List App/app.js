97e8eb5b5e38872e1eca2aa6ffbf49c88d1288fd
ZeusDeev <zeus.cach@outlook.com>
2025-08-17 16:44:18 -0500

    creating logic of de window message

--- To-Do List App/app.js
+++ To-Do List App/app.js
@@ -1,13 +1,27 @@
 const inputTex = document.getElementById('input-box');
 const button = document.querySelector('button');
 const listContainer = document.querySelector('.list-container')
+const alerta = document.querySelector('.is-hidden')
 
 function addTask() {
     if (inputTex.value === '') { //Si el valor del input text esta vacio
-        console.log("No puedes agregar valores vacios")
+        //console.log("No puedes agregar valores vacios")
+        message();
     } else {
         let li = document.createElement('li'); //Crea un nuevo nodo(elemento html)
         li.textContent = inputTex.value //textContent coloca el texto segun el valor proporcionado
         listContainer.appendChild(li);//añade el nodo creado con los valores a nuestro container
     }
+}
+
+
+//Crea Logica de mensaje
+function message() {
+    //document.getElementById('alerta').style.display = 'block';
+    alerta.classList.remove('is-hidden');//classlist.remove selecciona nuestra clase y la elimina
+    setTimeout(() => {
+        //tiempo de espera a agregar is-hidden(elimina el bloque)
+        alerta.classList.add('is-hidden')//classlist.add selecciona nuestra clase y añade
+    }, 2000);
+
 }
