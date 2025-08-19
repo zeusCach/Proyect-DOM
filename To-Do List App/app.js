80bd8a6c3dfb21d49e526a99614a0b5a3249805a
ZeusDeev <zeus.cach@outlook.com>
2025-08-19 01:28:05 -0500

    Creacion de funcion para eliminar tareas, ademas de funcionalidades extras

--- To-Do List App/app.js
+++ To-Do List App/app.js
@@ -3,16 +3,31 @@ const button = document.querySelector('button');
 const listContainer = document.querySelector('.list-container')
 const alerta = document.querySelector('.is-hidden')
 
+
+
 function addTask() {
     if (inputTex.value === '') { //Si el valor del input text esta vacio
+        
         //console.log("No puedes agregar valores vacios")
         message();
-    } else {
-        let li = document.createElement('li'); //Crea un nuevo nodo(elemento html)
-        li.textContent = inputTex.value //textContent coloca el texto segun el valor proporcionado
-        listContainer.appendChild(li);//añade el nodo creado con los valores a nuestro container
 
-        checkTask();
+    } else {
+
+        let deleteButton = document.createElement('span'); //Crea span para eliminar tarea
+        let li = document.createElement('li'); //Crea un nuevo nodo(elemento html)
+
+        li.textContent = inputTex.value //textContent coloca el texto segun el valor proporcionado
+        deleteButton.textContent = '❌';//añadimos con texContent una x(donde la x se coloca en el span, deleteButton)
+
+        deleteButton.classList.add('delete-task')//Añade clase a nuesto deleteButton
+
+        listContainer.appendChild(li);//añade el nodo creado con los valores a nuestro container
+        li.appendChild(deleteButton);//añadimos el nodo creado a nuestro li
+
+        inputTex.value = ''//Refresca nuestro buscador y elimina el texto ingresado en el input
+
+        checkTask();//funcion de check inchek
+        deleteTask()//Funcion que contiene logica para eliminar tarea
     }
 }
 
@@ -27,12 +42,37 @@ function message() {
 
 }
 
+//Crea Logica para seleccionar y deseleccionar tarea
 function checkTask() {
+    //Escuchamos el evento click de listContainer donde se encuentra nuestro li
     listContainer.addEventListener("click", e => {
-
+        //Accedemos a e.target(etiqueta li) para comprobar si existe la clase checked en li
         e.target.classList.contains("checked")
-            ? e.target.classList.remove('checked')
-            : e.target.classList.add('checked')
+            ? e.target.classList.remove('checked') //si existe la elimina
+            : e.target.classList.add('checked') //Si no existe la añade
 
     })
+
+    //classList recibe varios parametos, donde podemos hacer uso de el
 }
+
+function deleteTask() {
+    listContainer.addEventListener('click', e => {
+        //Comprueba si en nuestro elemento padre existe delete-task
+        if (e.target.classList.contains('delete-task')) {
+            e.target.parentElement.remove(); //si existe elimina el elemento padre en el que existe
+        }
+    })
+}
+
+//Funcion Main
+function main() {
+    //comprobamos si se hizo enter sobre nuestro input al ingresar un texto escuchando el evento keydown
+    inputTex.addEventListener('keydown', e => {
+        if (e.key === "Enter") {
+            addTask(); //si se hizo enter, añade la clase
+        }
+    })
+}
+
+main();//Ejecuta todo el programa
